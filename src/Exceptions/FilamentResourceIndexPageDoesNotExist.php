16e5c40471b8196c90bb19c8d1e3ad294120bad1
Brandon Ferens <brandon@kirschbaumdevelopment.com>
2025-06-11 03:57:32 -0700

    Paginated data in the resource data tool (#7)
    * #6: Paginated data in the resource data tool
    
    Updated .gitignore as well
    
    * Fix styling
    
    * Removed redundant argument
    
    * Fix styling

---
+++ src/Exceptions/FilamentResourceIndexPageDoesNotExist.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Kirschbaum\Loop\Filament\Exceptions;
+
+use Exception;
+
+class FilamentResourceIndexPageDoesNotExist extends Exception {}
--- src/GetFilamentResourceDataTool.php
+++ src/GetFilamentResourceDataTool.php
@@ -3,6 +3,9 @@
 namespace Kirschbaum\Loop\Filament;
 
 use Exception;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\PageRegistration;
+use Filament\Resources\Resource;
 use Filament\Tables\Columns\Column;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
@@ -11,7 +14,9 @@ use Kirschbaum\Loop\Concerns\Makeable;
 use Kirschbaum\Loop\Contracts\Tool;
 use Kirschbaum\Loop\Exceptions\LoopMcpException;
 use Kirschbaum\Loop\Filament\Concerns\ProvidesFilamentResourceInstance;
+use Kirschbaum\Loop\Filament\Exceptions\FilamentResourceIndexPageDoesNotExist;
 use Prism\Prism\Tool as PrismTool;
+use Throwable;
 
 class GetFilamentResourceDataTool implements Tool
 {
@@ -23,19 +28,17 @@ class GetFilamentResourceDataTool implements Tool
         return app(PrismTool::class)
             ->as($this->getName())
             ->for('Gets the data for a given Filament resource, applying optional filters (try to use them). Always call the describe_filament_resource tool before calling this tool. Always try to use the available filters to get the data you need.')
-            ->withStringParameter('resource', 'The resource class name of the resource to get data for, from the list_filament_resources tool.', required: true)
+            ->withStringParameter('resource', 'The resource class name of the resource to get data for, from the list_filament_resources tool.')
             ->withStringParameter('filters', 'JSON string of filters to apply (e.g., \'{"status": "published", "author_id": [1, 2]}\').', required: false)
-            ->using(function (string $resource, ?string $filters = null) {
+            ->withNumberParameter('perPage', 'The resource data is paginated. This is the number of records per page. It defaults to 10', required: false)
+            ->withNumberParameter('page', 'The resource data is paginated. This is the page the paginated results should be from.', required: false)
+            ->using(function (string $resource, ?string $filters = null, ?int $perPage = 10, ?int $page = null) {
                 $resource = $this->getResourceInstance($resource);
                 $filters = $this->parseFilters($filters);
 
                 try {
-                    $listPageClass = $resource::getPages()['index'];
-                    $component = $listPageClass->getPage();
+                    $listPage = $this->getListPage($resource);
 
-                    /** @var InteractsWithTable $listPage */
-                    $listPage = new $component;
-                    $listPage->bootedInteractsWithTable();
                     $table = $listPage->getTable();
                     $tableColumns = $table->getColumns();
 
@@ -69,42 +72,50 @@ class GetFilamentResourceDataTool implements Tool
                         }
                     }
 
-                    // TODO: Allow the tool to specify the number of results to return with a max
-                    $results = $listPage->getFilteredTableQuery()->take(10)->get();
+                    $results = $listPage->getFilteredTableQuery()->paginate(perPage: $perPage, page: $page);
 
-                    $outputData = $results->map(function (Model $model) use ($tableColumns) {
-                        $rowData = [
-                            $model->getKeyName() => $model->getKey(),
-                        ];
+                    $outputData = [
+                        'data' => $results->getCollection()
+                            ->map(function (Model $model) use ($tableColumns) {
+                                $rowData = [
+                                    $model->getKeyName() => $model->getKey(),
+                                ];
 
-                        foreach ($tableColumns as $column) {
-                            /** @var Column $column */
-                            $columnName = $column->getName();
+                                foreach ($tableColumns as $column) {
+                                    /** @var Column $column */
+                                    $columnName = $column->getName();
 
-                            try {
-                                if (str_contains($columnName, '.')) {
-                                    $relationName = strtok($columnName, '.');
+                                    try {
+                                        if (str_contains($columnName, '.')) {
+                                            $relationName = strtok($columnName, '.');
 
-                                    if (method_exists($model, $relationName)) {
-                                        $model->loadMissing($relationName);
-                                        $value = data_get($model, $columnName);
-                                    } else {
-                                        $value = null;
-                                        Log::warning("Relation '{$relationName}' not found on model for column '{$columnName}'.");
+                                            if (method_exists($model, $relationName)) {
+                                                $model->loadMissing($relationName);
+                                                $value = data_get($model, $columnName);
+                                            } else {
+                                                $value = null;
+                                                Log::warning("Relation '{$relationName}' not found on model for column '{$columnName}'.");
+                                            }
+                                        } else {
+                                            $value = $model->getAttribute($columnName);
+                                        }
+
+                                        $rowData[$columnName] = $value;
+                                    } catch (Exception $e) {
+                                        $rowData[$columnName] = null;
+                                        Log::error("Could not retrieve value for column '{$columnName}' on model ID {$model->getKey()}': {$e->getMessage()}");
                                     }
-                                } else {
-                                    $value = $model->getAttribute($columnName);
                                 }
 
-                                $rowData[$columnName] = $value;
-                            } catch (Exception $e) {
-                                $rowData[$columnName] = null;
-                                Log::error("Could not retrieve value for column '{$columnName}' on model ID {$model->getKey()}': {$e->getMessage()}");
-                            }
-                        }
+                                return $rowData;
+                            }),
 
-                        return $rowData;
-                    });
+                        'pagination' => [
+                            'total' => $results->total(),
+                            'per_page' => $results->perPage(),
+                            'current_page' => $results->currentPage(),
+                        ],
+                    ];
 
                     return json_encode($outputData);
                 } catch (Exception $e) {
@@ -143,5 +154,36 @@ class GetFilamentResourceDataTool implements Tool
         }
 
         return $filters;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    protected function getListPage(Resource $resource): ListRecords
+    {
+        /**
+         * @var ?PageRegistration $listPageClass
+         */
+        $listPageClass = data_get($resource::getPages(), 'index');
+
+        throw_unless(
+            $listPageClass instanceof PageRegistration,
+            FilamentResourceIndexPageDoesNotExist::class,
+            'No index page exists for ['.get_class($resource).']'
+        );
+
+        /**
+         * @var class-string<ListRecords> $component
+         */
+        $component = $listPageClass->getPage();
+
+        /**
+         * @var ListRecords $listPage
+         */
+        $listPage = new $component;
+
+        $listPage->bootedInteractsWithTable();
+
+        return $listPage;
     }
 }
